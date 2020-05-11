3637697c45192c3527cee0e0b420a5492fc110be
Max Davitt <max@davitt.me>
2020-05-11 15:35:59 -0400

    Revise untyped `on` code and docs
    I toyed with implementing support for [emittery](https://npm.im/emittery), but implementing typing and handling how it emitted events using promises would've greatly complicated our code. It did make me think more about how custom emitters are free to set their own return types for methods (looking at you `Emittery.Typed<..., ...>::once`), and I decided it would be safest to use `.on` and `.off` instead of `.once` for our untyped implementation to help alleviate issues that may arise from that.

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import {loopWhile} from 'deasync';
 
 /**
- * Constructs a tuple of arguments from function `T` (used with event handler types). Stolen from [`typed-emitter`](https://github.com/andywer/typed-emitter/blob/2942782ce940518e2e0841ac117ef178268ef493/index.d.ts#L1-L3). Smarter than builtin [`Parameters<T>`](https://www.typescriptlang.org/docs/handbook/utility-types.html#parameterst), similar to `ArgsN` from [`tsargs`](https://github.com/Morglod/tsargs)?
+ * Constructs a tuple of arguments from function `T` (used with event handler types). Stolen from [`typed-emitter`](https://github.com/andywer/typed-emitter/blob/2942782ce940518e2e0841ac117ef178268ef493/index.d.ts#L1-L3). Similar to builtin [`Parameters<T>`](https://www.typescriptlang.org/docs/handbook/utility-types.html#parameterst) and `ArgsN` from [`tsargs`](https://github.com/Morglod/tsargs)?
  */
 export type Arguments<T> = [T] extends [(...args: infer U) => any]
 	? U
@@ -11,20 +11,25 @@ export type Arguments<T> = [T] extends [(...args: infer U) => any]
 
 /**
  * Untyped synchronous function to wait on an event from an emitter.
- * @param emitter Anything with a callable property named `once` (unchecked), invoked as: `emitter.once(event, (...args: any[]): void => { ... })`
- * @param event Anything that can be used as a valid event type (unchecked), used for: `emitter.once(event, ...)`
+ * @param emitter Anything with synchronously callable properties `on` and `off` (unchecked), invoked like: `emitter.on(event, listener)` and `emitter.off(event, listener)` where `listener: (...args: any[]) => void`. 
+ * @param event Anything that can be used as a valid event type (unchecked), invoked like: `emitter.on(event, ...)` and `emitter.off(event, ...)`.
  * @returns An array of the arguments the event was emitted with.
  */
 export default function on(emitter: any, event: any): any {
 	let values: any[] | undefined;
 	let emitted = false;
 
-	emitter.once(event, (...args: any[]): void => {
+	const listener = (...args: any[]): void => {
+		values = emitted ? values : args;
 		emitted = true;
-		values = args;
-	});
+	};
+
+	// The call signature for .once isn't universal amongst custom emitters, but .on/.off typically are
+	emitter.on(event, listener);
 
 	loopWhile(() => !emitted);
+
+	emitter.off(event, listener);
 
 	return values;
 }
