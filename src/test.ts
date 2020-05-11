e26b8079d32d21114e152df147460a1664de41e7
Max Davitt <max@davitt.me>
2020-05-11 15:42:41 -0400

    Implement basic typecheck tests
    Basically, this allows us to ensure that unexpected types from our supported typed EventEmitter implementations cause tests to break.

--- src/test.ts
+++ src/test.ts
@@ -1,31 +1,41 @@
 import {EventEmitter} from 'events';
+import StrictEventEmitter from 'strict-event-emitter-types';
+import {DefaultEventMap, EventEmitter as TypedEventEmitter} from 'tsee';
+import TypedEmitter from 'typed-emitter';
 
-import on from '.';
+import onEventEmitter from '.';
+import onStrictEventEmitter from './strict-event-emitter-types';
+import onTypedEventEmitter from './tsee';
+import onTypedEmitter from './typed-emitter';
+
+interface TestEventMap extends DefaultEventMap {
+	e: (n: number, s: string) => void;
+}
 
 describe('on(...)', () => {
-	test('waits for emit', async () => {
+	test('waits for emit', () => {
 		const ee = new EventEmitter();
 
 		setImmediate(() => ee.emit('e', null));
 
-		const emitted = on(ee, 'e');
+		const emitted = onEventEmitter(ee, 'e');
 
 		expect(emitted).toEqual(expect.arrayContaining([null]));
 		expect(emitted).toHaveLength(1);
 	});
 
-	test('does not return undefined', async () => {
+	test('does not return undefined', () => {
 		const ee = new EventEmitter();
 
 		setImmediate(() => ee.emit('e'));
 
-		const emitted = on(ee, 'e');
+		const emitted = onEventEmitter(ee, 'e');
 
 		expect(emitted).not.toEqual(undefined);
 		expect(emitted).toHaveLength(0);
 	});
 
-	test('returns first emitted event', async () => {
+	test('returns first emitted event', () => {
 		const ee = new EventEmitter();
 
 		setImmediate(() => {
@@ -33,20 +43,56 @@ describe('on(...)', () => {
 			ee.emit('e', 2);
 		});
 
-		const emitted = on(ee, 'e');
+		const emitted = onEventEmitter(ee, 'e');
 
 		expect(emitted).toEqual(expect.arrayContaining([1]));
 		expect(emitted).toHaveLength(1);
 	});
 
-	test('returns all arguments of emitted event', async () => {
+	test('returns all arguments of emitted event', () => {
 		const ee = new EventEmitter();
 
 		setImmediate(() => ee.emit('e', 1, 2));
 
-		const emitted = on(ee, 'e');
+		const emitted = onEventEmitter(ee, 'e');
 
 		expect(emitted).toEqual(expect.arrayContaining([1, 2]));
+		expect(emitted).toHaveLength(2);
+	});
+
+	test('typechecks for StrictEventEmitter<..., ...>', () => {
+		const ee: StrictEventEmitter<
+			EventEmitter,
+			TestEventMap
+		> = new EventEmitter();
+
+		setImmediate(() => ee.emit('e', 0, ''));
+
+		const emitted: [number, string] = onStrictEventEmitter(ee, 'e');
+
+		expect(emitted).toEqual(expect.arrayContaining([0, '']));
+		expect(emitted).toHaveLength(2);
+	});
+
+	test('typechecks for TypedEventEmitter<...>', () => {
+		const ee = new TypedEventEmitter<TestEventMap>();
+
+		setImmediate(() => ee.emit('e', 0, ''));
+
+		const emitted: [number, string] = onTypedEventEmitter(ee, 'e');
+
+		expect(emitted).toEqual(expect.arrayContaining([0, '']));
+		expect(emitted).toHaveLength(2);
+	});
+
+	test('typechecks for TypedEmitter<...>', () => {
+		const ee = new EventEmitter() as TypedEmitter<TestEventMap>;
+
+		setImmediate(() => ee.emit('e', 0, ''));
+
+		const emitted: [number, string] = onTypedEmitter(ee, 'e');
+
+		expect(emitted).toEqual(expect.arrayContaining([0, '']));
 		expect(emitted).toHaveLength(2);
 	});
 });
